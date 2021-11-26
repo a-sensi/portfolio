55c4d7ee9d89a0678c7edec548de741e088bbd7a
a-sensi <asensi@artic.edu>
2021-11-25 21:14:21 -0600

    add comic pages

--- docs/scripts/footer.js/main.js
+++ docs/scripts/footer.js/main.js
@@ -4,9 +4,13 @@ let mainFooter = `
       <h5>alexsensiba@gmail.com</h5>
       <div class="social" >
         <a href="https://www.instagram.com/alxdae/"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c521.png" alt=""></a>
-        <a href="https://twitter.com/alxdae"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt=""></a>
+        <a href="#"><img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c53e.png" alt=""></a>
       </div>
     </div>
 </div>`
 
 $('.footer').append(mainFooter);
+
+
+
+// https://twitter.com/alxdae
