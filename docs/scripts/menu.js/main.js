adf5fa5e68a689ef85001cd9bc1d18bcce86de26
a-sensi <asensi@artic.edu>
2021-11-29 20:30:32 -0600

    ow menu + page numbers

--- docs/scripts/menu.js/main.js
+++ docs/scripts/menu.js/main.js
@@ -1,23 +1,23 @@
 let mainMenu = `
 <div class="icon">
         <div="home-button">
-          <img src="https://bl6pap003files.storage.live.com/y4mxxcG2x73ldYr8-xef-pkyGSUT5VUHKl0lP_XHGyrGpWVyt920SsqUQRrGBfADv4-2-6f3mYMRF36TbTT3KKEa9RybifXxNhK2P0CxsCTiIlxkjXlEkLhjxv6ProSYqucMHbyNStDJSKzI9snVWksOcNqPx-MJQo_DNhpxox08NE0H6jWrRjdM8gTo3grTY55?width=500&height=500&cropmode=none" alt="">
-      
+          <a href="#"> <img src="https://bl6pap003files.storage.live.com/y4mxxcG2x73ldYr8-xef-pkyGSUT5VUHKl0lP_XHGyrGpWVyt920SsqUQRrGBfADv4-2-6f3mYMRF36TbTT3KKEa9RybifXxNhK2P0CxsCTiIlxkjXlEkLhjxv6ProSYqucMHbyNStDJSKzI9snVWksOcNqPx-MJQo_DNhpxox08NE0H6jWrRjdM8gTo3grTY55?width=500&height=500&cropmode=none" alt="">
+          <p class="text"> MENU</p> </a>
+        
           <div>
             <p class="top"></p>
             <nav class="menu">
              <ul>
-                <li> <a class="home" href="/docs/index.html">Home</a></li>
-                
-                  <nav class="sub-menu">
+                <a class="home" href="/docs/index.html">Home</a>
                     <a class="illu" href="/docs/illustration">Illustration</a>
                     <a class="comic" href="/docs/comics">Comics</a>
                     <a class="ani" href="/docs/animation">Animation</a>
-                  </nav>
+                    <a class="ow" href="/docs/otherworld/cover-1">OtherWorld</a>
+                    <a class="bio" href="/docs/bio/">Bio</a>
+                    <a class="archive" href="/docs/archive">Archive</a>
+                 
                 </li>
-                <li><a class="ow" href="/docs/otherworld/cover-1">OtherWorld</a></li>
-                <li><a class="bio" href="/docs/bio/">Bio</a></li>
-                <li><a class="archive" href="/docs/archive">Archive</a></li>
+              
               </ul>
             </nav>
       
