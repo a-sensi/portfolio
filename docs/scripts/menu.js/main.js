1f07073a701a1c5cf01f3a8e06a108a1d20bce6b
a-sensi <asensi@artic.edu>
2021-12-04 21:00:45 -0600

    house

--- docs/scripts/menu.js/main.js
+++ docs/scripts/menu.js/main.js
@@ -1,5 +1,5 @@
 let mainMenu = `
-<a class="home" href="/portfolio/index.html"><div id="home-two"><img src="https://bl6pap003files.storage.live.com/y4mQ8T_TNzXam4Uqg0zMc_4Fw7gCCqWJM-wpLEIqvmYp0bloUH9M9odjkNsHZJht_hZ40eqk4FMI4vqfuFcC6CZLJ4MWicT05d5uFD2usXu48jTwjtJKZu7-cdQBProKOMrDNsJaGPK0GIs6TdvPmTh3eBb7d3rD3Zw0JXU6XGkspHSzb_U3azs1OZzBxiA5BVZ?width=500&height=500&cropmode=none" alt=""></div>
+<a class="home-house" href="/portfolio/index.html"><div id="home-two"><img src="https://bl6pap003files.storage.live.com/y4mQ8T_TNzXam4Uqg0zMc_4Fw7gCCqWJM-wpLEIqvmYp0bloUH9M9odjkNsHZJht_hZ40eqk4FMI4vqfuFcC6CZLJ4MWicT05d5uFD2usXu48jTwjtJKZu7-cdQBProKOMrDNsJaGPK0GIs6TdvPmTh3eBb7d3rD3Zw0JXU6XGkspHSzb_U3azs1OZzBxiA5BVZ?width=500&height=500&cropmode=none" alt=""></div>
     <div id="home-one"><img src="https://bl6pap003files.storage.live.com/y4m-ZPf1_3QsKBRb1yHPu9BcT4_Ch4Onj5pfB1NE0uVxKJ8pOOVkTUF_EFCJM6peCvnK8YyA59CZ56OmuMZ7LebUFbF6UgxRv3FTG7AAOBFGzoBf4Tp-0yPkVShDvlCDh9GtliSAHiULysHqCMxVS3jAbtEoyQDiRDPesV5NeN179tNkQjJnLWfDEVnE2-MSMiv?width=500&height=500&cropmode=none" alt=""></div></a>
 <div class="icon">
         <div="home-button">
