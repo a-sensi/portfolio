4839329d1d327a50cd808954a80ab7801fc18b2f
a-sensi <asensi@artic.edu>
2021-12-04 21:56:22 -0600

    ow

--- docs/scripts/ow-menu.js/main.js
+++ docs/scripts/ow-menu.js/main.js
@@ -1,7 +1,7 @@
 let mainMenu = `
 <div class=title>
 <div class="ow">
-  <img src="https://bl6pap003files.storage.live.com/y4m1ceYDHX1UTMurJlAWuhJHHNHT3sXy2IG6F4vnGWKkn_RnAztC8rBOKeBkAgId0DRjJLb2ZRGtceQqe6OJaPlz1nHkI6Zp-j95PHnFifKWshxSAALcYEVJVKgeEIr_y9GD-AFgCnXHhH66pPgtJa-BMelf7uOPeBF6HotHRj-LOsRfbj5H9zqNHrrja1k-f4e?width=909&height=228&cropmode=none" alt="">
+<a href="/portfolio/otherworld/cover-1"><img src="https://bl6pap003files.storage.live.com/y4m1ceYDHX1UTMurJlAWuhJHHNHT3sXy2IG6F4vnGWKkn_RnAztC8rBOKeBkAgId0DRjJLb2ZRGtceQqe6OJaPlz1nHkI6Zp-j95PHnFifKWshxSAALcYEVJVKgeEIr_y9GD-AFgCnXHhH66pPgtJa-BMelf7uOPeBF6HotHRj-LOsRfbj5H9zqNHrrja1k-f4e?width=909&height=228&cropmode=none" alt=""></a>
 </div>
 
 <nav class="ow-menu-block">
