1b3a528c7d1d53b11764a9c2ad352f68a50ff50e
a-sensi <asensi@artic.edu>
2021-12-13 19:03:38 -0600

    dfasdfa s

--- docs/scripts/menu.js/main.js
+++ docs/scripts/menu.js/main.js
@@ -1,5 +1,5 @@
 let mainMenu = `
-<a  href="/portfolio/index.html"><div class="home-house"></div></a>
+<a  href="/index.html"><div class="home-house"></div></a>
 <div class="icon">
         <div class="home-button">
           <a href="#"> <img src="https://bl6pap003files.storage.live.com/y4mxxcG2x73ldYr8-xef-pkyGSUT5VUHKl0lP_XHGyrGpWVyt920SsqUQRrGBfADv4-2-6f3mYMRF36TbTT3KKEa9RybifXxNhK2P0CxsCTiIlxkjXlEkLhjxv6ProSYqucMHbyNStDJSKzI9snVWksOcNqPx-MJQo_DNhpxox08NE0H6jWrRjdM8gTo3grTY55?width=500&height=500&cropmode=none" alt="">
@@ -10,12 +10,12 @@ let mainMenu = `
             <nav class="menu">
              <ul>
           
-                    <a class="illu" href="/portfolio/illustration/">Illustration</a>
-                    <a class="comic" href="/portfolio/comics/">Comics</a>
-                    <a class="ani" href="/portfolio/animation">Animation</a>
-                    <a class="ow" href="/portfolio/otherworld/cover-1">OtherWorld</a>
-                    <a class="bio" href="/portfolio/bio/">Bio</a>
-                    <a class="archive" href="/portfolio/archive">Archive</a>
+                    <a class="illu" href="/illustration/">Illustration</a>
+                    <a class="comic" href="/comics/">Comics</a>
+                    <a class="ani" href="/animation">Animation</a>
+                    <a class="ow" href="/otherworld/cover-1">OtherWorld</a>
+                    <a class="bio" href="/bio/">Bio</a>
+                    <a class="archive" href="/archive">Archive</a>
                  
                 </li>
               
