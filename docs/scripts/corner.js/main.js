dc4408ec88097b3d939972897fac7690f81f2028
a-sensi <asensi@artic.edu>
2021-12-14 15:30:33 -0600

    asfds

--- docs/scripts/corner.js/main.js
+++ docs/scripts/corner.js/main.js
@@ -2,7 +2,7 @@ let mainLogo = `
 
 <div class="name-logo">
     <div>
-    <img class="img-corner" src="/docs/scripts/corner.js/image/name-02.png" alt="">
+    <img class="img-corner" src="https://bl6pap003files.storage.live.com/y4mkn6fprlWc0EBAzmYkUSefIh9EI-EjI1AAoqn_x--SywjAhxwHFWe00FBSCSP1kDUIYIySLdKBywE9T6ALCzjUceR_zpiVh6JJZRKtKM2eMR7eCVaEX8Ud4jcxlItdiKevgdNOSpX_nJ7T5nbejm1foCriZR-cFAo4NM33Z0-ABEVnBXaCAYRFOaYzHcR3tVC?width=1108&height=645&cropmode=none" alt="">
   </div>
 </div>
 
