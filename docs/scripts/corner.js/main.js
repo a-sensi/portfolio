d4000e54e213a662d7d3c0d0e3bcb754b8066dda
a-sensi <asensi@artic.edu>
2021-12-14 14:02:19 -0600

    akldjfs

--- docs/scripts/corner.js/main.js
+++ docs/scripts/corner.js/main.js
@@ -2,7 +2,7 @@ let mainLogo = `
 
 <div class="name-logo">
     <div>
-     <img class="img-corner" src="https://bl6pap003files.storage.live.com/y4mylEljnB_5ZWcf5YWw_HuhjvVMIYZuU-H1nxu15n7IBzF7ugcQkFj10oYqpa473_EBTk99ntVNWujNSpnWd5memekdnS1QeoIWjyVtW3hr80hbzEI-82iu56rwPbWs-sHtsf8IT6bJNcOF9JSN0xgIYSFYdfvtIlXcC52Iy5_I3OB93-aEQ3jrLeIRHS25CZk?width=3300&height=2213&cropmode=none" alt="">
+    <img class="img-corner" src="/docs/scripts/corner.js/image/name-02.png" alt="">
   </div>
 </div>
 
