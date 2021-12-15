76464cf88ce3ee2b0a3014504f0eb20daf1613f5
a-sensi <asensi@artic.edu>
2021-12-14 19:54:06 -0600

    home?

--- docs/scripts/menu.js/main.js
+++ docs/scripts/menu.js/main.js
@@ -1,5 +1,5 @@
 let mainMenu = `
-<a  href="/index.html"><div class="home-house"></div></a>
+<a  href="https://www.alexsensiba.com/"><div class="home-house"></div></a>
 <div class="icon">
         <div class="home-button">
           <a href="#"> <img src="https://bl6pap003files.storage.live.com/y4mw7OueLfDhSV5Hk-QXLUkx1HZlQRIRZTs01w0DeMD2v2-fBZLMN70XxlgRkvCwWJwwu7b9_0ol8hU4NGWyrC74S9fSUybrStlDOEma3UctH9c6_RlW_aIMI3o4yslOkMA3ebay7JwsObN_2pYweHodG-NGdP5GpZpIIjmauC2T3nBEH9811JN8VryrDRJto4Z?width=500&height=500&cropmode=none" alt="">
